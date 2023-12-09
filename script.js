b0d85f7b6fd2e2ba7031a0134d2734c36898eac8
AndreaParrini <andrea.parrini26@gmail.com>
2023-12-09 11:34:19 +0100

    add row in html and create markup in script
    add row in html to stamp object
    crate markup i script to stamp in DOM

--- script.js
+++ script.js
@@ -9,32 +9,37 @@ const teamMember = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg'
+        image: 'https://picsum.photos/200/300'
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',	
-        image: 'angela-caroll-chief-editor.jpg'
+        /* image: 'angela-caroll-chief-editor.jpg' */
+        image: 'https://picsum.photos/200/300'
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',	
-        image: 'walter-gordon-office-manager.jpg'
+        /* image: 'walter-gordon-office-manager.jpg' */
+        image: 'https://picsum.photos/200/300'
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',	
-        image: 'angela-lopez-social-media-manager.jpg'
+        /* image: 'angela-lopez-social-media-manager.jpg' */
+        image: 'https://picsum.photos/200/300'
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',	
-        image: 'scott-estrada-developer.jpg'
+        /* image: 'scott-estrada-developer.jpg' */
+        image: 'https://picsum.photos/200/300'
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',	
-        image: 'barbara-ramos-graphic-designer.jpg'
+        /* image: 'barbara-ramos-graphic-designer.jpg' */
+        image: 'https://picsum.photos/200/300'
     }
 ]
 
@@ -47,8 +52,17 @@ Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e 
 
 for (let i = 0; i < teamMember.length; i++) {
     const infoOfMember = teamMember[i];
+    // assegno ad una costante le varie proprietà dell'oggetto
     const nameMember = infoOfMember.name;
     const roleMember = infoOfMember.role;
     const imageMember = infoOfMember.image;
+    // stampo in console le tre variabile create
     console.log(nameMember, roleMember, imageMember);
+
+    // recupero il nodo della DOM dove inserire le stringhe
+    const rowEl = document.querySelector('.container .row');
+    // creo il markup del div da inserire nella DOM
+    const divMarkup = `<div>Nome: ${nameMember} Ruolo: ${roleMember} Image: <img src="${imageMember}" ></div>`
+    // inserisdo in html il markup creato
+    rowEl.insertAdjacentHTML('beforeend', divMarkup);
 }
